a47e1dffe63b869fc792665e5a46785cfe712e48
Stephen Breuhaus <sbreuhaus@gmail.com>
2016-04-17 23:14:10 -0400

    Revert "more nonworking input fields for edit"

--- frontend/app/stateless/EditProjectUI.js
+++ frontend/app/stateless/EditProjectUI.js
@@ -38,30 +38,6 @@ const EditProjectUI = (props) => {
           value={props.newSkills}>
         </input>
         <br/>
-        <input
-          className='projectInput'
-          placeholder="Description"
-          onChange={props.onInput}
-          name="newSkills">
-
-        </input>
-        <br/>
-        <input
-          className='projectInput'
-          placeholder="Image (900x506)"
-          onChange={props.onInput}
-          name="newSkills">
-
-        </input>
-        <br/>
-        <input
-          className='projectInput'
-          placeholder="Link"
-          onChange={props.onInput}
-          name="newSkills">
-
-        </input>
-        <br/>
         <button type="button" onClick={props.onEditProject}>Edit Project</button>
       </div>
     </div>
